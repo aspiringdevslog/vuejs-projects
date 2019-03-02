a9171070a66b7dbbd3cd322fb0d77816595969fc
aspiringdevslog <aspiringdevslog@gmail.com>
2019-03-02 19:23:23 +0800

    split into components

--- firstapp/src/components/VuetifyDataTable.vue
+++ firstapp/src/components/VuetifyDataTable.vue
@@ -3,58 +3,11 @@
 <v-app light class="inspire">
 	<div class="vuetify-data-table">
 
-		<v-btn color="warning" dark class="mb-2" v-on:click="clearLocal('desserts')">Clear Data</v-btn>
+		<data-menu>
+			
+		</data-menu>
 
-
-
-
-		<v-dialog v-model="dialog" max-width="80%">
-			<template v-slot:activator="{ on }">
-				<v-btn color="primary" dark class="mb-2" v-on="on">Add Dessert</v-btn>
-			</template>
-
-			<v-card>
-				<v-card-title>
-					<span class="headline">{{ formTitle }}</span>
-				</v-card-title>
-
-				<v-card-text>
-					<v-container grid-list-md>
-						<v-layout wrap>
-							<v-flex xs12 sm6 md4>
-								<!-- <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field> -->
-								<v-autocomplete
-								label="Dessert name"
-								v-model="editedItem.name"
-								:items="dessertList"
-								>
-<!-- 									<template v-slot:append-outer>
-										<v-slide-x-reverse-transition
-										mode="out-in"
-										>
-										</v-slide-x-reverse-transition>
-									</template> -->
-								</v-autocomplete>
-							</v-flex>
-
-							<v-flex xs12 sm6 md4>
-								<v-text-field v-model="editedItem.price" label="Price"></v-text-field>
-							</v-flex>
-							
-							<v-flex xs12 sm6 md4>
-								<v-text-field v-model="editedItem.desc" label="Description"></v-text-field>
-							</v-flex>
-						</v-layout>
-					</v-container>
-				</v-card-text>
-
-				<v-card-actions>
-					<v-spacer></v-spacer>
-					<v-btn color="blue darken-1" flat v-on:click="close">Cancel</v-btn>
-					<v-btn color="blue darken-1" flat v-on:click="save">Save</v-btn>
-				</v-card-actions>
-			</v-card>
-		</v-dialog>
+		<ranked-data-menu></ranked-data-menu>
 
 		<hr>
 		<v-data-table
@@ -97,9 +50,9 @@
 		>
 			<template v-slot:items="props">
 				<!-- <td></td> -->
-				<td>{{ props.item.first }}</td>
-				<td>{{ props.item.second }}</td>
-				<td>{{ props.item.third }}</td>
+				<td>{{ props.item.a }}</td>
+				<td>{{ props.item.b }}</td>
+				<td>{{ props.item.c }}</td>
 				<td>
 		          <v-icon
 		            small
@@ -114,51 +67,6 @@
 		</v-data-table>
 		<br>
 
-		<v-card>
-			<v-card-title>
-				<span class="headline">{{ formTitle }}</span>
-			</v-card-title>
-
-			<v-card-text>
-				<v-container grid-list-md>
-					<v-layout wrap>
-						<v-flex xs12 sm6 md4>
-							<v-autocomplete
-							label="First Dessert"
-							v-model="rankedDesserts.first"
-							:items="dessertList"
-							>
-							</v-autocomplete>
-						</v-flex>
-
-						<v-flex xs12 sm6 md4>
-							<v-autocomplete
-							label="Second Dessert"
-							v-model="rankedDesserts.second"
-							:items="dessertList"
-							>
-							</v-autocomplete>
-						</v-flex>
-						
-						<v-flex xs12 sm6 md4>
-							<v-autocomplete
-							label="Third Dessert"
-							v-model="rankedDesserts.third"
-							:items="dessertList"
-							>
-							</v-autocomplete>
-						</v-flex>
-					</v-layout>
-				</v-container>
-			</v-card-text>
-
-			<v-card-actions>
-				<v-spacer></v-spacer>
-				<!-- <v-btn color="blue darken-1" flat v-on:click="close">Cancel</v-btn> -->
-				<v-btn color="blue darken-1" flat v-on:click="saveRanked">Save</v-btn>
-			</v-card-actions>
-		</v-card>
-
 		
 	</div>
 </v-app>
@@ -166,7 +74,15 @@
 </template>
 
 <script>
+
+import DataMenu from './DataMenu.vue'
+import RankedDataMenu from './RankedDataMenu.vue'
+
 	export default {
+		components: {
+			'data-menu': DataMenu,
+			'ranked-data-menu': RankedDataMenu
+		},
 		data: function(){
 			return {
 				rankingHeaders: [
@@ -200,11 +116,8 @@
 					{"text":"$vuetify.dataIterator.rowsPerPageAll","value":-1}
 				],
 				rankings: [
-					// {
-					// 	first: "Ice Cream",
-					// 	second: "Chocolate",
-					// 	third: "Mango Pudding"
-					// }
+
+
 				],
 				headers: [
 					{
@@ -247,8 +160,9 @@
 		      this.dialog = true
 		    },
 		    deleteRankedItem (item) {
-		      const index = this.rankedDesserts.indexOf(item)
-		      confirm('Are you sure you want to delete this item?') && this.rankedDesserts.splice(index, 1)
+		      const index = this.rankings.indexOf(item)
+		      confirm('Are you sure you want to delete this item?') && this.rankings.splice(index, 1)
+		      localStorage.set('rankings', JSON.stringify(this.rankings));
 		    },
 		    deleteItem (item) {
 		      const index = this.desserts.indexOf(item)
@@ -324,8 +238,8 @@
 	    beforeMount() {
 	    	console.log(this.desserts);
 	    }
-
 	}
+
 </script>
 
 <style scoped>
